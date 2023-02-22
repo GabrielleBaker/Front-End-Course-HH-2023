5675899539861cc763b3088d0fedf4078185377c
Gabrielle Baker <98098568+GabrielleBaker@users.noreply.github.com>
2023-02-22 12:13:52 +0200

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react';
+import Todotable from './components/Todolist';
 
 function App() {
+  const[todo, setTodo]= useState({desc: '',date: ''});
+  const[todos, setTodos]= useState([]);
+
+  const inputChanged=(event)=>{
+    event.preventDefault();
+    setTodo({...todo,[event.target.name]: event.target.value});
+}
+  const addTodo = (event) =>{
+    event.preventDefault();
+    setTodos([...todos,todo]);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Simple Todo List</h1>
+      <h2>Gabrielle Baker</h2>
       </header>
+     
+      <form onSubmit={addTodo} class="form">
+      <label>Add todo:</label>
+      <br></br>
+        <label>Description: </label>
+        <input type="date" name="date" value={todo.date} onChange={inputChanged}/>
+
+        <label>Date: </label>
+        <input type="text" name ="desc" value={todo.desc} onChange={inputChanged}/>
+        <input type="submit" value="Add"/>
+      </form>
+      <Todotable todos={todos} />
+  
     </div>
   );
 }
